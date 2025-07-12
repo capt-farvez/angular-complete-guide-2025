48b10984ae44cc7a33384fc31ada085f63a131c9
capt-farvez <farvez.capt@gmail.com>
2025-07-12 23:43:37 +0600

    Show users image and name , using signals

--- 02-Task-Management-App/task-management-app/src/app/user/user.ts
+++ 02-Task-Management-App/task-management-app/src/app/user/user.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
+    selectedUser = signal(DUMMY_USERS[randomIndex]);
+    imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+    // imagePath = computed(() => `assets/users/user-1.jpg`);
 
+    // get imagePath() {
+    //   return 'assets/users/' + this.selectedUser.avatar
+    // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
