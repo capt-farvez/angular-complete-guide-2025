f48cc738ed978170efa0d4e843d5e60915548348
capt-farvez <farvez.capt@gmail.com>
2025-07-13 17:46:17 +0600

    Showing user name on click user card

--- 02-Task-Management-App/task-management-app/src/app/app.ts
+++ 02-Task-Management-App/task-management-app/src/app/app.ts
@@ -1,14 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { User } from './user/user';
+import { Tasks } from './tasks/tasks';
+
+import { DUMMY_USERS } from './user/dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ Header, User ],
+  imports: [ Header, User, Tasks],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('task-management-app');
+
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- 02-Task-Management-App/task-management-app/src/app/user/user.ts
+++ 02-Task-Management-App/task-management-app/src/app/user/user.ts
@@ -1,25 +1,21 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
 export class User {
-    selectedUser = signal(DUMMY_USERS[randomIndex]);
-    imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-    // imagePath = computed(() => `assets/users/user-1.jpg`);
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-    // get imagePath() {
-    //   return 'assets/users/' + this.selectedUser.avatar
-    // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
