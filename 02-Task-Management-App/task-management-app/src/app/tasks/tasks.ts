182b3459cd76d9b61bb1d2f76390bdf0ae5114fa
capt-farvez <farvez.capt@gmail.com>
2025-07-15 15:12:15 +0600

    Add new task functionalities

--- 02-Task-Management-App/task-management-app/src/app/tasks/tasks.ts
+++ 02-Task-Management-App/task-management-app/src/app/tasks/tasks.ts
@@ -1,18 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Task } from "./task/task";
+import { NewTask } from './new-task/new-task';
 
 import { DUMMY_TASKS } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask = false;
+
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks(){
@@ -21,5 +25,13 @@ export class Tasks {
 
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
