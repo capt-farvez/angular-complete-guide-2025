3d24600c86570bd34d1b658210ae15ee22524511
capt-farvez <farvez.capt@gmail.com>
2025-07-15 14:09:37 +0600

    Deleteing task

--- 02-Task-Management-App/task-management-app/src/app/tasks/task/task.ts
+++ 02-Task-Management-App/task-management-app/src/app/tasks/task/task.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { TaskInterface } from './task.model';
 
@@ -11,4 +11,9 @@ import { TaskInterface } from './task.model';
 
 export class Task {
   @Input({ required:true}) task!: TaskInterface;
+  @Output() complete= new EventEmitter<string>();
+
+  onCompleteTask(){
+    this.complete.emit(this.task.id);
+  }
 }
