abf2115b6e32d5b0388894df4a792d51c72bd07a
capt-farvez <farvez.capt@gmail.com>
2025-07-21 15:35:51 +0600

    Shared components : button  and  control

---
+++ 04-Ticket-App/ticket-app/src/app/shared/control/control.ts
@@ -0,0 +1,43 @@
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  ViewEncapsulation,
+  contentChild,
+  inject,
+  input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.html',
+  styleUrl: './control.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
+})
+export class Control {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+  label = input.required<string>();
+  private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control());
+  }
+}
